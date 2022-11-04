1aa9a3bdee4ef9e1c8c7042b163c244b0bcb58dd
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-04 13:21:10 +1100

    Need to change code to include settings

--- GenAuthentication.Api/Controllers/LoginController.cs
+++ GenAuthentication.Api/Controllers/LoginController.cs
@@ -1,6 +1,129 @@
-﻿namespace GenAuthentication.Api.Controllers
+﻿using GenCore.Data.Extensions;
+using GenCore.Data.Models;
+using GenCore.Data.Repositories.Interface;
+using GenCryptography.Service.Utilities.Interface;
+using GenNotification.Service.Utilities.Interface;
+using GenTokenization.Service.Utilities.Interface;
+using GenValidation.Service.Utilities.Interface;
+using Microsoft.AspNetCore.Mvc;
+using static Dapper.SqlMapper;
+
+namespace GenAuthentication.Api.Controllers
 {
-    public class LoginController
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class LoginController : ControllerBase
     {
+        private readonly IUserVerificationRepository _userVerificationRepo;
+        private readonly IUserEncryptionRepository _userEncryptionRepo;
+        private readonly IUserTokensRepository _userTokensRepo;
+        private readonly IKeyGenerator _keyGenerator;
+        private readonly IEncryptor _encryptor;
+        private readonly ITokenizer _alphanumericTokenizer;
+        private readonly ITokenizer _numericTokenizer;
+        private readonly IEmailDespatcher _emailDespatcher;
+        private readonly ISmsDespatcher _smsDespatcher;
+
+        public LoginController(IKeyGenerator keyGenerator, IEncryptor encryptor, ITokenizer tokenizer, IUserVerificationRepository userVerificationRepository, IUserEncryptionRepository userEncryptionRepository, IUserTokensRepository userTokensRepository)
+        {
+            _keyGenerator = keyGenerator;
+            _encryptor = encryptor;
+            _alphanumericTokenizer = _numericTokenizer = tokenizer;
+            _userVerificationRepo = userVerificationRepository;
+            _userEncryptionRepo = userEncryptionRepository;
+            _userTokensRepo = userTokensRepository;
+        }
+
+        [HttpPost(Name = "Individual")]
+        public IActionResult Individual([FromBody] User user)
+        {
+            UserVerification userVerification = _userVerificationRepo.Get(user.Username);
+
+            if (userVerification is null)
+            {
+                return BadRequest("User not found");
+            }
+
+            userVerification.Username = user.Username;
+
+            string userVerificationJson = userVerification.ToJson();
+
+            if (userVerificationJson.IsEmpty())
+            {
+                return BadRequest("User could not be verified");
+            }
+
+            byte[] encryptionKey = _keyGenerator.GenerateEncryptionKey();
+
+            if ((encryptionKey is null) || (encryptionKey.Count() == 0))
+            {
+                return BadRequest("Failed to generate encryption key");
+            }
+
+            _userEncryptionRepo.InsertOrUpdate(userVerification.UserId, encryptionKey);
+
+            string encryptedUserVerificationJson = _encryptor.Encrypt(encryptionKey, userVerificationJson);
+
+            if (encryptedUserVerificationJson.IsEmpty())
+            {
+                return BadRequest("User verification could not be processed");
+            }
+
+            if (!userVerification.EmailVerified)
+            {
+                _emailDespatcher.SendEmail(Settings.SmtpHost, Settings.SmtpPort, Settings.SmtpUseSsl, userVerification.Email, Settings.SmtpSender, Settings.SmtpPassword, "Verification required", $"Please verify email at https://localhost:7138/api/Authentication/Verify?mode=email&user={encryptedUserVerificationJson}");
+            }
+
+            if (!userVerification.PhoneVerified)
+            {
+                _smsDespatcher.SendSms(Settings.ClickSendUsername, Settings.ClickSendApiKey, userVerification.Phone, Settings.SmsSender, $"Please verify phone number at https://localhost:7138/api/Authentication/Verify?mode=phone&user={encryptedUserVerificationJson}");
+            }
+
+            if (!(userVerification.EmailVerified || userVerification.PhoneVerified))
+            {
+                return BadRequest("Either email or phone needs to be verified to access content");
+            }
+
+            UserToken userToken = new UserToken()
+            { 
+                UserId = userVerification.UserId,
+                Username = userVerification.Username,
+                Email = userVerification.Email,
+                Phone = userVerification.Phone,
+                Role = userVerification.Role,
+                LastActive = userVerification.LastActive
+            };
+
+            if (userVerification.EmailVerified)
+            {
+                userToken.Token = _alphanumericTokenizer.GetUniqueKey(Settings.EmailValidationSize);
+
+                if (userToken.Token.IsEmpty())
+                {
+                    return BadRequest("Error while generating token");
+                }
+
+                string validationMessage = $"Please use the following token, which expires in 24 hours, to login: {userVerification.Token}";
+
+                _emailDespatcher.SendEmail(Settings.SmtpHost, Settings.SmtpPort, Settings.SmtpUseSsl, userVerification.Email, Settings.SmtpSender, Settings.SmtpPassword, "Validate login attempt", validationMessage);
+            }
+            else if (userVerification.PhoneVerified)
+            {
+                userToken.Token = _numericTokenizer.GetUniqueKey(Settings.PhoneValidationSize);
+
+                if (userToken.Token.IsEmpty())
+                {
+                    return BadRequest("Error while generating token");
+                }
+
+                string validationMessage = $"Please use the following token, which expires in 24 hours, to login: {userVerification.Token}";
+
+                _smsDespatcher.SendSms(Settings.ClickSendUsername, Settings.ClickSendApiKey, userVerification.Phone, Settings.SmsSender, validationMessage);
+            }
+
+            _userTokensRepo.InsertOrUpdate(userVerification.UserId, userToken.Token);
+
+            return Ok(userToken.Token);
+        }
     }
 }
--- GenAuthentication.Api/Program.cs
+++ GenAuthentication.Api/Program.cs
@@ -1,5 +1,8 @@
 using GenCore.Data.Repositories.Implementation;
 using GenCore.Data.Repositories.Interface;
+using GenCryptography.Service.Utilities.Interface;
+using GenNotification.Service.Utilities.Interface;
+using GenTokenization.Service.Utilities.Interface;
 using GenValidation.Service.Utilities.Interface;
 
 string connectionString = "Data Source=localhost;Initial Catalog=CwRetail;Persist Security Info=true;User ID=TestLogin; Password = ABC123";
@@ -14,10 +17,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddTransient<IValidator>();
+builder.Services.AddTransient<IKeyGenerator>();
+builder.Services.AddTransient<IEncryptor>();
+builder.Services.AddTransient<ITokenizer>();
+builder.Services.AddTransient<IEmailDespatcher>();
+builder.Services.AddTransient<ISmsDespatcher>();
 builder.Services.AddTransient<IUserRepository>(s => new UserRepository(connectionString));
 builder.Services.AddTransient<IRolesRepository>(s => new RolesRepository(connectionString));
 builder.Services.AddTransient<IUserVerificationRepository>(s => new UserVerificationRepository(connectionString));
 builder.Services.AddTransient<IUserRolesRepository>(s => new UserRolesRepository(connectionString));
+builder.Services.AddTransient<IUserEncryptionRepository>(s => new UserEncryptionRepository(connectionString));
+builder.Services.AddTransient<IUserTokensRepository>(s => new UserTokensRepository(connectionString));
 
 var app = builder.Build();
 
