2639327b2ad038202ab8e82a626f4fc84cae30e9
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-08 18:26:55 +1100

    Import router

--- GenAuthentication.Ui/src/app/app.module.ts
+++ GenAuthentication.Ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { NotFoundComponent } from './not-found/not-found.component';
--- GenAuthentication.Ui/src/app/login/login.component.ts
+++ GenAuthentication.Ui/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from '../../models/user';
 import { AuthenticationService } from '../../services/authentication.service';
 
