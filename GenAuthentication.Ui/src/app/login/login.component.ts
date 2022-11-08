1b8b6f6613013fd6741c01e89d5d562a230846b1
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-08 17:55:03 +1100

    Populate components with content

--- GenAuthentication.Ui/src/app/login/login.component.ts
+++ GenAuthentication.Ui/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../../models/user';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Settings } from '../../settings';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  protected user: User = new User();
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService, private settings: Settings, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.user = new User();
   }
 
+  get(username: string): void {
+    this.authenticationService.getUser(new User(0n, username))
+      .then(() => {
+        this.goToValidate();
+      })
+      .catch((error) => {
+        console.log("Promise rejected with " + JSON.stringify(error));
+      });
+  }
+
+  goToValidate(): void {
+    const navigationDetails: string[] = ['/validate'];
+    this.router.navigate(navigationDetails);
+  }
+
+  goToRegister(): void {
+    const navigationDetails: string[] = ['/register'];
+    this.router.navigate(navigationDetails);
+  }
 }
--- GenAuthentication.Ui/src/app/register/register.component.ts
+++ GenAuthentication.Ui/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../../models/user';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Settings } from '../../settings';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  protected user: User = new User();
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService, private settings: Settings, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.user = new User();
   }
 
+  add(username: string, email: string, phone: string): void {
+    this.authenticationService.addUser(new User(0n, username, email, phone))
+      .then(() => {
+        this.goToLogin();
+      })
+      .catch((error) => {
+        console.log("Promise rejected with " + JSON.stringify(error));
+      });
+  }
+
+  goToLogin(): void {
+    const navigationDetails: string[] = ['/login'];
+    this.router.navigate(navigationDetails);
+  }
 }
